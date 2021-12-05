14cdaa7e9ec585c7e4ef8acc45c364599e3046b3
Sijia Ge <90580930+Blaze998@users.noreply.github.com>
2021-12-05 01:56:49 -0600

    Update vocabulary_size_postgres.py

--- vocabulary_size_postgres.py
+++ vocabulary_size_postgres.py
@@ -27,25 +27,36 @@ conn = psycopg2.connect(database="final_project", user="gb760")
 cur = conn.cursor()
 # execute a SQL command
 query = """
-select * from tweets;
+select time_stamp from tweets order by 1 desc limit 1;
 """
 cur.execute(query)
+tm = []
+tm = cur.fetchall()
+t = tm[0][0]
+start_of_current = ceil_dt(t, timedelta(minutes=-1)) 
+end_of_current = t
 
-#get the tweets content list - each row of the table tweets is a tuple
-tweets_lst = cur.fetchall()
-time_lst = [tweets_rows[1] for tweets_rows in tweets_lst]
-text_lst = [tweets_rows[2] for tweets_rows in tweets_lst]
+# open a connection (close it later)
+conn = psycopg2.connect(database="final_project", user="gb760")
+# create a cursor
+cur = conn.cursor()
+# execute a SQL command
+query = """
+select * from tweets where time_stamp>='"+str(start_of_current)+"' and time_stamp<='"+str(end_of_current)+"';
+"""
+cur.execute(query)
+tweets = []
+tweets = cur.fetchall()
+time_lst = [tweets[x][1] for x in range(len(tweets))]
+text_lst = [tweets[x][2] for x in range(len(tweets))]
+
 
 #repeating the same progress in part C
 def ceil_dt(dt, delta):
     return datetime.min + math.ceil((dt - datetime.min) / delta) * delta
 
-t = time_lst[0]+timedelta(minutes = 2)
-start_of_current = ceil_dt(t, timedelta(minutes=-1)) 
-end_of_current = ceil_dt(t, timedelta(minutes=1)) - timedelta(seconds = 1)
 
-
-def current_text(text, start_of_current, end_of_current):
+def current_text_fnc(text, start_of_current, end_of_current):
     current_text = []
     for i in range(len(text)):
         if time_lst[i]>=start_of_current and time_lst[i]<=end_of_current:
@@ -53,7 +64,7 @@ def current_text(text, start_of_current, end_of_current):
     return current_text
 
 #unprocessed tweet text list of current minute at t
-current_text = current_text(text_lst, start_of_current, end_of_current)
+current_text = current_text_fnc(text_lst, start_of_current, end_of_current)
 
 #def vocabulary_size()
 def vocabulary_size(text):
@@ -63,9 +74,20 @@ def vocabulary_size(text):
     tweet_words = tweets_string.split(' ') #split the string into a list with space.
     tweet_words = list(filter(lambda a: a != '', tweet_words)) #drop the ''.
     tweet_words = [item for item in tweet_words if item[0] != '#'] #drop the remaining sign '#'.
-    uniq_words = set(tweet_words)
-    uniq_count = len(uniq_words)
+    tweet_words_set = set(tweet_words)
+    words_set = len(tweet_words_set)
+    
+    phrase_list = []
+    for i in range(len(tweet_words)-1):
+        phrase_list.append(tweet_words[i:i+2])
+    phrase_list_set = set(tuple(x) for x in phrase_list)
+    phrases = len(phrase_list)-len(text)+1
+    phrases_set = len(phrase_list_set)-len(text)+1
+    
+    uniq_count = words_set+phrases_set
+
     return uniq_count
+
 
 #get the number of unique words in the current minute at t
 unique_word_count = vocabulary_size(current_text)
@@ -73,38 +95,26 @@ unique_word_count = vocabulary_size(current_text)
 #print the frequency of p in the current minute at t
 print("The number of unique words used in the tweets posted in the current minute t = ", str(t), "is", unique_word_count, "!")
 
-# reset uniq_word_count table
-def reset_uniq_word_count_table():
-    connection = psycopg2.connect(user="gb760", dbname = "final_project")
-    cursor = connection.cursor()
-    cursor.execute("""TRUNCATE uniq_word_count""")
-    connection.commit()
-    if connection:
-        cursor.close()
-        connection.close()
 
-reset_uniq_word_count_table()  
-
-
-# insert values into uniq_word_count table
+# insert values into unique_words_current_count table
 def insert_value(insert_query_values):
     connection = psycopg2.connect(user="gb760", dbname = "final_project")
     cursor = connection.cursor()              
-    query = """INSERT INTO uniq_word_count (t,start_of_current_minute,end_of_current_minute,uniq_word_count)
-VALUES (%s,%s,%s,%s)"""           
+    query = """INSERT INTO unique_words_current_count (start_of_current_minute,uniq_wph_current_count)
+VALUES (%s,%s)"""           
     cursor.execute(query, insert_query_values)
     connection.commit()           
     if connection:
         cursor.close()
         connection.close()
 
-insert_query_values = (t,start_of_current,end_of_current,unique_word_count)
+insert_query_values = (end_of_current,unique_word_count)
 insert_value(insert_query_values)
-print('Successfully insert all information needed for calculating the number of unique words at time t!')
+print('Insertion into table done!')
 
 
-# print the record of the uniq_word_count table -- not necessarily to print this.
-cur.execute("SELECT * FROM uniq_word_count")
+# print the record of the unique_words_current_count table -- not necessarily to print this.
+cur.execute("SELECT * FROM unique_words_current_count")
 records = cur.fetchall()
 print(records)
 conn.commit()
