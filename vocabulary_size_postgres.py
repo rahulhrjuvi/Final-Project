90a425864c4ee375fd2f8dcc8fcc6721215c039f
Ruiyu98 <90641035+Ruiyu98@users.noreply.github.com>
2021-12-05 11:26:42 -0600

    fix issues before run
    1. fix issues of psycopg to psycopg2
    2. tidy up the codes
    3. fix issues of insert into schema table

--- vocabulary_size_postgres.py
+++ vocabulary_size_postgres.py
@@ -1,4 +1,7 @@
-#importing libraries
+# Milestone 2 Part D vocabulary_size_postgres.py
+
+###################### import required packages ######################
+
 import sys
 import re
 import math
@@ -6,9 +9,9 @@ from datetime import datetime, timedelta
 import numpy as np
 import psycopg2
 
+###################### defined functions ######################
 
-# Cleaning the tweets
-# reference link: https://gist.github.com/aniruddha27/8d112b87ff4014b80f606dc68080066d#file-preprocess_tweets-py
+#repeating the same three defined funcitons in Milestone 2 Part C: preprocess(), ceil_dt(), current_text_fnc()
 def preprocess(tweet):
     # remove links
     tweet = re.sub(r'http\S+', '', tweet)
@@ -21,40 +24,8 @@ def preprocess(tweet):
     tweet = tweet.lower()
     return tweet
 
-# open a connection (close it later)
-conn = psycopg2.connect(database="final_project", user="gb760")
-# create a cursor
-cur = conn.cursor()
-# execute a SQL command
-query = """
-select time_stamp from tweets order by 1 desc limit 1;
-"""
-cur.execute(query)
-tm = []
-tm = cur.fetchall()
-t = tm[0][0]
-start_of_current = ceil_dt(t, timedelta(minutes=-1)) 
-end_of_current = t
-
-# open a connection (close it later)
-conn = psycopg2.connect(database="final_project", user="gb760")
-# create a cursor
-cur = conn.cursor()
-# execute a SQL command
-query = """
-select * from tweets where time_stamp>='"+str(start_of_current)+"' and time_stamp<='"+str(end_of_current)+"';
-"""
-cur.execute(query)
-tweets = []
-tweets = cur.fetchall()
-time_lst = [tweets[x][1] for x in range(len(tweets))]
-text_lst = [tweets[x][2] for x in range(len(tweets))]
-
-
-#repeating the same progress in part C
 def ceil_dt(dt, delta):
     return datetime.min + math.ceil((dt - datetime.min) / delta) * delta
-
 
 def current_text_fnc(text, start_of_current, end_of_current):
     current_text = []
@@ -63,17 +34,14 @@ def current_text_fnc(text, start_of_current, end_of_current):
             current_text.append(text[i])
     return current_text
 
-#unprocessed tweet text list of current minute at t
-current_text = current_text_fnc(text_lst, start_of_current, end_of_current)
-
-#def vocabulary_size()
+#define function for vocabulary size
 def vocabulary_size(text):
     #get a list of processed words.
     tweets_string = ' '.join(text) 
-    tweets_string = preprocess(tweets_string) #put all processed tweets texts into a single string.
-    tweet_words = tweets_string.split(' ') #split the string into a list with space.
-    tweet_words = list(filter(lambda a: a != '', tweet_words)) #drop the ''.
-    tweet_words = [item for item in tweet_words if item[0] != '#'] #drop the remaining sign '#'.
+    tweets_string = preprocess(tweets_string)
+    tweet_words = tweets_string.split(' ')
+    tweet_words = list(filter(lambda a: a != '', tweet_words))
+    tweet_words = [item for item in tweet_words if item[0] != '#']
     tweet_words_set = set(tweet_words)
     words_set = len(tweet_words_set)
     
@@ -88,15 +56,7 @@ def vocabulary_size(text):
 
     return uniq_count
 
-
-#get the number of unique words in the current minute at t
-unique_word_count = vocabulary_size(current_text)
-
-#print the frequency of p in the current minute at t
-print("The number of unique words used in the tweets posted in the current minute t = ", str(t), "is", unique_word_count, "!")
-
-
-# insert values into unique_words_current_count table
+#define function for storing each record in schema table unique_words_current_count
 def insert_value(insert_query_values):
     connection = psycopg2.connect(user="gb760", dbname = "final_project")
     cursor = connection.cursor()              
@@ -108,17 +68,37 @@ VALUES (%s,%s)"""
         cursor.close()
         connection.close()
 
+###################### main part ######################
+
+conn = psycopg2.connect(database="final_project", user="gb760")
+cur = conn.cursor()
+query = """select time_stamp from tweets order by 1 desc limit 1;"""
+cur.execute(query)
+tm = cur.fetchall()
+cur.close()
+conn.close()
+
+t = tm[0][0]
+start_of_current = ceil_dt(t, timedelta(minutes=-1)) 
+end_of_current = t
+
+conn = psycopg2.connect(database="final_project", user="gb760")
+cur = conn.cursor()
+query = "select * from tweets where time_stamp>='"+str(start_of_current)+"' and time_stamp<='"+str(end_of_current)+"';"
+cur.execute(query)
+tweets = cur.fetchall()
+cur.close()
+conn.close()
+
+time_lst = [tweets[x][1] for x in range(len(tweets))]
+text_lst = [tweets[x][2] for x in range(len(tweets))]
+
+#get and print the number of unique words used in the tweets posted in the current minute at t
+current_text = current_text_fnc(text_lst, start_of_current, end_of_current)
+unique_word_count = vocabulary_size(current_text)
+print("The number of unique words used in the tweets posted in the current minute ", str(start_of_current), "is", unique_word_count, "!")
+
+# insert values into unique_words_current_count table
 insert_query_values = (end_of_current,unique_word_count)
 insert_value(insert_query_values)
 print('Insertion into table done!')
-
-
-# print the record of the unique_words_current_count table -- not necessarily to print this.
-cur.execute("SELECT * FROM unique_words_current_count")
-records = cur.fetchall()
-print(records)
-conn.commit()
-
-#close the cursor and connection to PostgreSQL.
-cur.close()
-conn.close()
